cf58f163676503ef9f7c422f38d0aa9c71d72677
pinya52 <a30515@gmail.com>
2026-02-24 22:54:43 +0800

    fix socket

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import numpy as np
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'pictureguess-secret-2024')
 CORS(app, resources={r"/*": {"origins": "*"}})
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
 game_state = {
     'status': 'waiting',
